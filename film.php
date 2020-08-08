9e6468c8725cd80d172b839e2beccd93625090e7
Javier <me@xavidiaz.com>
2020-08-08 12:02:06 +0200

    header + footer added each page

--- film.php
+++ film.php
@@ -50,48 +50,36 @@ if (isset($_GET["id"]) && !empty(trim($_GET["id"]))) {
     exit();
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<!-- HEADER -->
+<?include 'templates/header.php' ?>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Visa filmdetaljer</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        .wrapper {
-            width: 500px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h1><?php echo $row["title"]; ?></h1>
+<!-- HTML -->
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1><?php echo $row["title"]; ?></h1>
+                </div>
+                <div class="form-group">
+                    <div class="form-group">
+                        <label>Director</label>
+                        <p class="form-control-static"><?php echo $row["director"]; ?></p>
                     </div>
                     <div class="form-group">
-                        <div class="form-group">
-                            <label>Director</label>
-                            <p class="form-control-static"><?php echo $row["director"]; ?></p>
-                        </div>
-                        <div class="form-group">
-                            <label>Category</label>
-                            <p class="form-control-static"><?php echo $row['cat_name']; ?></p>
-                        </div>
-
-                        <div class="form-group">
-                            <label>Year</label>
-                            <p class="form-control-static"><?php echo $row["year"]; ?></p>
-                        </div>
-                        <p><a href="index.php" class="btn btn-primary">bakåt</a></p>
+                        <label>Category</label>
+                        <p class="form-control-static"><?php echo $row['cat_name']; ?></p>
                     </div>
+
+                    <div class="form-group">
+                        <label>Year</label>
+                        <p class="form-control-static"><?php echo $row["year"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">bakåt</a></p>
                 </div>
             </div>
         </div>
-</body>
+    </div>
 
-</html>
+    <!-- FOOTER -->
+    <?include 'templates/footer.php' ?>
---
+++ templates/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+
+    <title>Fimlbibliotek</title>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style type="text/css">
+        .wrapper {
+            width: 650px;
+            margin: 0 auto;
+        }
+
+        .page-header h2 {
+            margin-top: 0;
+        }
+
+        table tr td:last-child a {
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+
+<body>
