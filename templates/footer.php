a885d3d776c9803112312759aed68189b7107469
Javier <me@xavidiaz.com>
2020-08-08 11:09:21 +0200

    Revert "index.php styled + tooltips"
    This reverts commit 459df46f72c19ec5f1735228f9949e8b51d312ff.

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,5 @@
 <!--JavaScript at end of body for optimized loading-->
 <script type="text/javascript" src="js/materialize.min.js"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.tooltipped');
-        M.Tooltip.init(elems, {});
-    });
-</script>
 </body>
 
 </html>
