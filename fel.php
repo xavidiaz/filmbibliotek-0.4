6f747f883929532b6373211ae3efa9ffd5c623c5
Javier <me@xavidiaz.com>
2020-08-09 12:15:22 +0200

    Done! Now time for comments and documentation

--- fel.php
+++ fel.php
@@ -2,20 +2,21 @@
 <?include 'templates/header.php' ?>
 
 <!-- HTML -->
-<div class="wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="page-header">
-                    <h1>Ogiltig begäran</h1>
-                </div>
-                <div class="alert alert-danger fade in">
-                    <p>Tyvärr, ogiltig begäran. Snälla du <a href="index.php" class="alert-link">gå tillbaka </a> och försök igen.</p>
-                </div>
+<div class="container ">
+    <div class="row py-3">
+        <div class="col col-8 mx-auto">
+            <div class="pull-left pl-3 pb-2">
+                <h2>Ogiltig begäran</h2>
             </div>
+
+            <div class="alert alert-danger px-auto py-4 text-center">
+
+                <h4>Tyvärr, ogiltig begäran. Snälla du <a href="index.php" class="alert-link">gå tillbaka </a> och försök igen.</h4>
+
+            </div>
+
         </div>
     </div>
 </div>
-
 <!-- FOOTER -->
 <?include 'templates/footer.php' ?>
--- templates/header.php
+++ templates/header.php
@@ -14,31 +14,33 @@
     <!-- Material Icons -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined">
 
+    <!-- Custom Css -->
+    <!--  <link rel="stylesheet" href="css/style.css"> -->
+
 
 
 </head>
 
 <body>
     <header>
+
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="index.php">Fimlbibliotek</a>
+            <div class="container">
+                <a class="navbar-brand" href="index.php">Fimlbibliotek</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        </nav>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="index.php">Fimlbibliotek</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a href="skapa.php" class="btn btn-outline-dark ">Lägg till ny film</a>
+                        </li>
 
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a href="skapa.php" class="btn btn-outline-dark ">Lägg till ny film</a>
-                    </li>
+                    </ul>
+                </div>
 
-                </ul>
-
-            </div>
         </nav>
 
     </header>
+    <main>
